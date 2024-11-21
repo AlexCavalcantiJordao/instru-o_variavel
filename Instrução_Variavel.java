e77020cd0a708215213f9fd772284af03021c5b9
AlexCavalcantiJordao <alex37705732@gmail.com>
2024-11-21 20:10:01 -0300

    instrução_variavel

---
+++ Instrução_Variavel.java
@@ -0,0 +1,21 @@
+package instrução_variavel;
+
+public class Instrução_Variavel {
+
+    public static void main(String[] args) {
+        
+        boolean VouF = true;
+        byte a;
+        char letra = 'F';
+        int valor = 4500;
+        double x = 3.14;
+        a = 20;
+        
+        System.out.printf("Valor VouF (boolean): %b\n",  VouF);
+        System.out.printf("Valor de a (byte): %d\n", a);
+        System.out.printf("Valor da letra (char): %c\n", letra);
+        System.out.printf("Valor de valor (int): %d\n", valor);
+        System.out.printf("Valor de X (double): %2.2f\n", x);
+        
+    }
+}
